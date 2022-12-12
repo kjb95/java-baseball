fd7c383c7134ab4d01b4d28cd3c8a27670376ebf
kjb95 <topmedia44@gmail.com>
2022-12-12 13:26:20 +0900

    feat: 입력에 따른 게임 새로 시작하기 또는 게임 종료

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -11,13 +11,22 @@ public class BaseballController {
     private final BaseballService baseballService = new BaseballService();
 
     public void run() {
+        boolean isRestart = true;
         OutputView.printStartGame();
-        baseballService.createBaseballAnswerNumber();
-        String number = InputView.requestNumberBaseball();
-        GameResultDto gameResultDto = baseballService.computeGameResult(number);
-        OutputView.printGameResult(gameResultDto.getBallCount(), gameResultDto.getStrikeCount());
-        if (gameResultDto.getStrikeCount() == Constant.BASEBALL_NUMBER_SIZE) {
-            boolean isRestartGame = InputView.requestRestartGame();
+        while (isRestart) {
+            isRestart = playGame();
         }
+    }
+
+    public boolean playGame() {
+        boolean isGameOver = false;
+        baseballService.createBaseballAnswerNumber();
+        while (!isGameOver) {
+            String number = InputView.requestNumberBaseball();
+            GameResultDto gameResultDto = baseballService.computeGameResult(number);
+            OutputView.printGameResult(gameResultDto.getBallCount(), gameResultDto.getStrikeCount());
+            isGameOver = gameResultDto.getStrikeCount() == Constant.BASEBALL_NUMBER_SIZE;
+        }
+        return InputView.requestRestartGame();
     }
 }
